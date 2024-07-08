199903846f605f8c32133134ad90bff4c98b3eb2
Alex Grytsai <a.grytsai1987@gmail.com>
2024-07-08 10:15:16 +0300

    made ManageUserView and endpoint for managing user info

--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from user.views import CreateUserView
+from user.views import ManageUserView
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="register"),
+    path("me/", ManageUserView.as_view(), name="me"),
 ]
 
 app_name = "user"
--- user/views.py
+++ user/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
 from user.serializers import UserSerializer
 
@@ -8,3 +9,17 @@ class CreateUserView(generics.CreateAPIView):
     API view for creating a new user.
     """
     serializer_class = UserSerializer
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """
+    API view for managing an existing user.
+    """
+    serializer_class = UserSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self):
+        """
+        Get the authenticated user.
+        """
+        return self.request.user
