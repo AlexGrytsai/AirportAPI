004286286488ee285c5bf61c61ca3529aa198185
Alex Grytsai <a.grytsai1987@gmail.com>
2024-07-08 18:21:17 +0300

    created Airplane and AirplaneTypo models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class AirplaneType(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Airplane(models.Model):
+    name = models.CharField(
+        max_length=255,
+        unique=True,
+        null=False,
+        blank=False
+    )
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+    airplane_type = models.ForeignKey(AirplaneType, on_delete=models.CASCADE)
+
+    @property
+    def total_seats(self):
+        return self.rows * self.seats_in_row
+
+    def __str__(self):
+        return f"Airplane {self.name} (total seats: {self.total_seats})"
