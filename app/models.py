76f92f5a75097b92c3040f076017c5fe3f4ab127
Alex Grytsai <a.grytsai1987@gmail.com>
2024-07-09 07:04:22 +0300

    added a new field in AirplaneType: code

--- app/models.py
+++ app/models.py
@@ -3,6 +3,10 @@ from django.db import models
 
 class AirplaneType(models.Model):
     name = models.CharField(max_length=255)
+    code = models.CharField(max_length=8, unique=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.code})"
 
 
 class Airplane(models.Model):
