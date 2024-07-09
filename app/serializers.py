b02c0c79e9fb9a95b4bac7db77453530c70a5d51
Alex Grytsai <a.grytsai1987@gmail.com>
2024-07-09 12:08:46 +0300

    created AirplaneSerializer and AirplaneListSerializer

--- app/serializers.py
+++ app/serializers.py
@@ -1,9 +1,29 @@
 from rest_framework import serializers
 
-from app.models import AirplaneType
+from app.models import AirplaneType, Airplane
 
 
 class AirplaneTypeSerializer(serializers.ModelSerializer):
     class Meta:
         model = AirplaneType
-        fields = ("id", "name")
+        fields = ("id", "name",)
+
+
+class AirplaneSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Airplane
+        fields = (
+            "id", "name", "code", "rows", "seats_in_row", "airplane_type",
+            "total_seats")
+
+
+class AirplaneListSerializer(serializers.ModelSerializer):
+    airplane_type = serializers.SlugRelatedField(
+        slug_field="name", read_only=True
+    )
+
+    class Meta:
+        model = Airplane
+        fields = (
+            "id", "name", "airplane_type",
+            "total_seats")
