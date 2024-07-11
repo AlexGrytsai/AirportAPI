6ae2fb1841a4a65e79a9efc56b35bc91341f1755
Alex Grytsai <a.grytsai1987@gmail.com>
2024-07-11 07:36:57 +0300

    made an AirplaneDetailSerializer

--- app/serializers.py
+++ app/serializers.py
@@ -13,8 +13,14 @@ class AirplaneSerializer(serializers.ModelSerializer):
     class Meta:
         model = Airplane
         fields = (
-            "id", "name", "code", "rows", "seats_in_row", "airplane_type",
-            "total_seats")
+            "id",
+            "name",
+            "code",
+            "rows",
+            "seats_in_row",
+            "airplane_type",
+            "total_seats"
+        )
 
 
 class AirplaneListSerializer(serializers.ModelSerializer):
@@ -25,5 +31,18 @@ class AirplaneListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Airplane
         fields = (
-            "id", "name", "airplane_type",
-            "total_seats")
+            "id", "name", "airplane_type", "total_seats"
+        )
+
+
+class AirplaneDetailSerializer(serializers.ModelSerializer):
+    airplane_type = serializers.SlugRelatedField(
+        slug_field="name", read_only=True
+    )
+
+    class Meta:
+        model = Airplane
+        fields = (
+            "id", "name", "code", "rows", "seats_in_row", "airplane_type",
+            "total_seats"
+        )
