189b186d3eee81db8ca590f1f489160936f0f93d
Alex Grytsai <a.grytsai1987@gmail.com>
2024-07-11 07:38:28 +0300

    added the AirplaneDetailSerializer and redefined permissions in AirplaneTypeViewSet

--- app/views.py
+++ app/views.py
@@ -1,8 +1,9 @@
-from rest_framework import generics, mixins, viewsets
+from rest_framework import mixins, viewsets
+from rest_framework.permissions import IsAdminUser
 
 from app.models import AirplaneType, Airplane
 from app.serializers import AirplaneTypeSerializer, AirplaneSerializer, \
-    AirplaneListSerializer
+    AirplaneListSerializer, AirplaneDetailSerializer
 
 
 class AirplaneTypeViewSet(
@@ -20,4 +21,11 @@ class AirplaneViewSet(viewsets.ModelViewSet):
     def get_serializer_class(self):
         if self.action == "list":
             return AirplaneListSerializer
+        if self.action == "retrieve":
+            return AirplaneDetailSerializer
         return AirplaneSerializer
+
+    def get_permissions(self):
+        if self.request.method in ("PUT", "PATCH", "DELETE", "POST"):
+            return (IsAdminUser(),)
+        return super().get_permissions()
