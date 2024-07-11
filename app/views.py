3155cf6df9d0a226e4fc951766119b8226d87e00
Alex Grytsai <a.grytsai1987@gmail.com>
2024-07-11 08:58:39 +0300

    added a feature for filtering crew with title, first and last name, ids

--- app/views.py
+++ app/views.py
@@ -1,9 +1,13 @@
+from typing import List
+
+from django.db.models import QuerySet
 from rest_framework import mixins, viewsets
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
 
 from app.models import AirplaneType, Airplane, Crew
 from app.serializers import AirplaneTypeSerializer, AirplaneSerializer, \
-    AirplaneListSerializer, AirplaneDetailSerializer, CrewSerializer, CrewListSerializer, CrewDetailSerializer
+    AirplaneListSerializer, AirplaneDetailSerializer, CrewSerializer, \
+    CrewListSerializer, CrewDetailSerializer
 
 
 class AirplaneTypeViewSet(
@@ -68,3 +72,31 @@ class CrewViewSet(viewsets.ModelViewSet):
         if self.action == "retrieve":
             return CrewDetailSerializer
         return CrewSerializer
+
+    @staticmethod
+    def _params_to_ints(qs: str) -> List[int]:
+        """Converts a list of string IDs to a list of integers"""
+        return [int(str_id) for str_id in qs.split(",")]
+
+    def get_queryset(self) -> QuerySet:
+        """
+        Returns the appropriate queryset based on the query parameters.
+        """
+        title = self.request.query_params.get("title")
+        crew_id = self.request.query_params.get("crew_id")
+        first_name = self.request.query_params.get("first_name")
+        last_name = self.request.query_params.get("last_name")
+
+        queryset = super(CrewViewSet, self).get_queryset()
+
+        if title:
+            queryset = queryset.filter(title__icontains=title)
+        if first_name:
+            queryset = queryset.filter(first_name__icontains=first_name)
+        if last_name:
+            queryset = queryset.filter(last_name__icontains=last_name)
+        if crew_id:
+            crew_ids = self._params_to_ints(crew_id)
+            queryset = queryset.filter(id__in=crew_ids)
+
+        return queryset
