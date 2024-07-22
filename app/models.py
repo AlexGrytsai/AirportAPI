0ef4086295976975655a8f1c418150240c460924
Alex Grytsai <a.grytsai1987@gmail.com>
2024-07-22 11:15:46 +0300

    fixed flake8 in the Ticket model

--- app/models.py
+++ app/models.py
@@ -156,8 +156,12 @@ class Order(models.Model):
 
 
 class Ticket(models.Model):
-    flight = models.ForeignKey(Flight, on_delete=models.CASCADE, related_name="tickets")
-    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="tickets")
+    flight = models.ForeignKey(
+        Flight, on_delete=models.CASCADE, related_name="tickets"
+    )
+    order = models.ForeignKey(
+        Order, on_delete=models.CASCADE, related_name="tickets"
+    )
     row = models.IntegerField()
     seat = models.IntegerField()
 
